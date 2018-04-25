c6e72aa2587e946c5ef4515f5f912d74e485b35f
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-04-25 13:04:38 +0300

    Initial commit. Add ChronoBankPlatform, default AtomicSwap contract. Setup dev env configs.

---
+++ truffle.js
@@ -0,0 +1,23 @@
+module.exports = {
+networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 5700000
+    },
+    sidechain: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 5700000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  migrations_directory: './migrations'
+}
