98c57a93a8957c1f5d259bd85f2ac99c63608ad0
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-04-26 08:37:39 +0300

    Add truffle config

--- truffle.js
+++ truffle.js
@@ -1,23 +1,38 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+function getWallet(){
+  return require('fs').readFileSync("./wallet.json", "utf8").trim();
+}
+
 module.exports = {
-networks: {
+  networks: {
+    rinkeby:{
+      network_id:4,
+      provider: new HDWalletProvider(getWallet(), "QWEpoi123", 'https://rinkeby.infura.io/'),
+      gas: 4700000,
+      gasPrice: 10000000000
+    },
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*', // Match any network id
-      gas: 5700000
+      network_id: "*" // Match any network id
+    },
+    chronobank: {
+      network_id: 456719,
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://private-full-geth-node.chronobank.io'),
+      port: 8545,
+      gas: 4700000
+    },
+    lx: {
+      network_id: 456719,
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','http://localhost:8545'),
+      port: 8545,
+      gas: 4700000
     },
     sidechain: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      gas: 5700000
+      network_id: "*",
+      provider: new HDWalletProvider(getWallet(),'QWEpoi123','http://127.0.0.1:8541'),
+      gas: 3144658,
+      gasPrice: 10000
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  migrations_directory: './migrations'
-}
+  }
+};
