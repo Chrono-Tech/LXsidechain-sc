21c3941deac763b28673710413d2749b62eefe5b
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-05-04 17:49:55 +0300

    MINT-1479 Contracts: add test contracts

---
+++ contracts/helper/FakeCoin.sol
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+pragma solidity ^0.4.23;
+
+
+// For testing purposes
+contract FakeCoin {
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    mapping (address => uint) public balanceOf;
+
+    function mint(address _to, uint _value) public {
+        balanceOf[_to] += _value;
+        emit Transfer(this, _to, _value);
+    }
+
+    function totalSupply() public pure returns (uint) {
+        return 0;
+    }
+
+    function symbol() public pure returns (string) {
+        return "FAKE";
+    }
+
+    function decimals() public pure returns (uint) {
+        return 4;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        return transferFrom(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        if (balanceOf[_from] < _value) {
+            return false;
+        }
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceEth(address _address) public view returns (uint) {
+        return _address.balance;
+    }
+
+    function() public payable {
+        if (msg.value == 0) {
+            revert();
+        }
+    }
+}
---
+++ contracts/helper/FakeCoin2.sol
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+pragma solidity ^0.4.23;
+
+
+import "./FakeCoin.sol";
+
+
+// For testing purposes.
+contract FakeCoin2 is FakeCoin {
+
+    function symbol() public pure returns (string) {
+        return "FAKE2";
+    }
+}
