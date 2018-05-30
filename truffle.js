d377814178df519e336e93d3f00a4719803c25e3
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-05-30 15:35:42 +0300

    Add chronobank-lx-test network

--- truffle.js
+++ truffle.js
@@ -11,20 +11,18 @@ module.exports = {
             network_id: "*" // Match any network id
         },
         "local": {
-            provider: new HDWalletProvider(getWallet(),'QWEpoi123','http://127.0.0.1:8540'),
+            provider: new HDWalletProvider(getWallet(),'QWEpoi123','http://127.0.0.1:8542'),
             network_id: "*", // Match any network id
-            gas: 0x222222
+            gas: 4700000
         },
         "chronobank-lx-prod": {
             network_id: 456719, // TODO
             provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://public-sidechain.chronobank.io'),
-            port: 8545,
             gas: 4700000
         },
         "chronobank-lx-test": {
-            network_id: 456719, // TODO
-            provider: new HDWalletProvider(getWallet(),'QWEpoi123','https://private-sidechain.chronobank.io'),
-            port: 8545,
+            network_id: 0x42,
+            provider: new HDWalletProvider(getWallet(),'QWEpoi123','http://35.196.17.40:8546'),
             gas: 4700000
         }
     },
