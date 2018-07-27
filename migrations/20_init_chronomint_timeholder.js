a0f9a4f637401dce45776dcc29fb3125dfec505a
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-07-27 20:21:33 +0300

    LABX-358 Update migrations

--- migrations/20_init_chronomint_timeholder.js
+++ migrations/20_init_chronomint_timeholder.js
@@ -4,48 +4,33 @@ const StorageManager = artifacts.require('StorageManager')
 const MultiEventsHistory = artifacts.require("MultiEventsHistory")
 const ERC20DepositStorage = artifacts.require("ERC20DepositStorage")
 const ChronoBankPlatform = artifacts.require('ChronoBankPlatform')
-const ChronoBankAssetProxy = artifacts.require('ChronoBankAssetProxy')
 const LXValidatorManager = artifacts.require("LXValidatorManager")
+const deployConfig = require("../deploy-config")
 const { basename, } = require("path")
 
-module.exports = (deployer, networks, accounts) => {
+module.exports = (deployer, network, accounts) => {
+	const config = deployConfig({ artifacts: artifacts, web3: web3, network: network, accounts: accounts })
+
 	deployer.then(async () => {
-		const TIME_SYMBOL = 'TIME'
-
-		let MINER_ADDRESS
-		const MINER_TOKEN_DEPOSIT_LIMIT = web3.toBigNumber("10000000") // TODO: setup needed value for miner limit
-
-		if (networks !== 'development') {
+		if (network !== 'development') {
 			throw "Check one more time all addresses and limits to ensure that all set up correctly. Comment this error after a check"
-		}
-
-		if (networks === 'development') {
-			MINER_ADDRESS = accounts[9] // TODO: setup for other networks separatly
 		}
 
 		const storageManager = await StorageManager.deployed()
 		await storageManager.giveAccess(TimeHolder.address, "Deposits")
 
 		const platform = await ChronoBankPlatform.deployed()
-		const timeAddress = await platform.proxies(TIME_SYMBOL)
+		const timeAddress = await platform.proxies(config.mining.tokenSymbol)
 		const timeHolder = await TimeHolder.deployed()
 		await timeHolder.init(timeAddress, TimeHolderWallet.address, ERC20DepositStorage.address, LXValidatorManager.address)
 
 		const history = await MultiEventsHistory.deployed()
 		await history.authorize(timeHolder.address)
 		await timeHolder.setupEventsHistory(history.address);
+		await timeHolder.setPrimaryMiner(config.mining.primaryValidator)
 
-		if (MINER_ADDRESS !== undefined) {
-			await timeHolder.setPrimaryMiner(MINER_ADDRESS)
-		}
-
-		if (networks === 'development') {
-			let time = ChronoBankAssetProxy.at(timeAddress)
-			await time.approve(timeHolder.address, web3.toBigNumber(2).pow(255), {from:MINER_ADDRESS});
-		}
-
-		if (networks !== 'development') {
-			await timeHolder.getMiningDepositLimits(timeAddress, MINER_TOKEN_DEPOSIT_LIMIT)
+		if (network !== 'development') {
+			await timeHolder.setMiningDepositLimits(timeAddress, config.mining.depositLimit)
 		}
 
 		console.log(`[MIGRATION] [${parseInt(basename(__filename))}] TimeHolder init: #done`)
